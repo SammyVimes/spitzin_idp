77267984a203e4714cf9a661bc8276c6188cbd82
Semyon <samvimes@yandex.ru>
2015-12-08 03:14:35 +0300

    .

--- map.cpp
+++ map.cpp
@@ -8,13 +8,8 @@
 #include <QResizeEvent>
 #include <math.h>
 
-Map::Map(QWidget *parent) : QWebView(parent), pendingRequests(0)
+Map::Map(QWidget *parent) : QWebView(parent)
 {
-    manager = new QNetworkAccessManager(this);
-    connect(manager, SIGNAL(finished(QNetworkReply*)),
-            this, SLOT(replyFinished(QNetworkReply*)));
-    connect(this,SIGNAL(reloadMap()),
-            this,SLOT(loadCoordinates()));
 }
 
 void Map::selectMarker(int pos)
@@ -31,50 +26,6 @@ void Map::selectMarker(int pos)
     page()->mainFrame()->evaluateJavaScript( scriptStr.join("\n") );
 }
 
-/*
- * Запрашивает координаты в формате CSV по указанному адресу, мспользуя
- * Google Maps API
- */
-void Map::geoCode(const QString& address)
-{
-    clearCoordinates();
-    QString requestStr( tr("http://maps.google.com/maps/geo?q=%1&output=%2&key=%3")
-            .arg(address)
-            .arg("csv")
-            .arg("ABQIAAAAO49nNEVnwxGsuIBDsUs1sBRMlhDm1om-sbK3hx0z3hRQyGwd6hSC3qtdFB_5tj9DICmkSlnZqX5hrw") );
-
-    manager->get( QNetworkRequest(requestStr) );
-    ++pendingRequests;
-}
-
-void Map::replyFinished(QNetworkReply *reply)
-{
-    QString replyStr( reply->readAll() );
-    QStringList coordinateStrList = replyStr.split(",");
-
-    if( coordinateStrList.size() == 4) {
-        QPointF coordinate( coordinateStrList[2].toFloat(),coordinateStrList[3].toFloat() );
-        coordinates << coordinate;
-    }
-
-    --pendingRequests;
-    if( pendingRequests<1 )
-        emit( reloadMap() );
-}
-
-void Map::loadCoordinates()
-{
-    QStringList scriptStr;
-    scriptStr << QString("map.panTo(new GLatLng(%1, %2));")
-                         .arg(coordinates.last().x())
-                         .arg(coordinates.last().y());
-    page()->mainFrame()->evaluateJavaScript( scriptStr.join("\n") );
-}
-
-void Map::clearCoordinates()
-{
-    coordinates.clear();
-}
 
 void Map::resizeEvent(QResizeEvent *event)
 {
--- map.h
+++ map.h
@@ -12,22 +12,11 @@ class Map : public QWebView
 public:
     Map(QWidget *parent=0);
     void selectMarker(int pos);
-public slots:
-    void replyFinished(QNetworkReply*);
-    void loadCoordinates();
-    void geoCode(const QString &address);
-    void clearCoordinates();
-
 protected:
     void resizeEvent(QResizeEvent *event);
 
 signals:
     void reloadMap();
-
-private:
-    QNetworkAccessManager *manager;
-    QList<QPointF> coordinates;
-    int pendingRequests;
 };
 
 #endif // MAP_H
